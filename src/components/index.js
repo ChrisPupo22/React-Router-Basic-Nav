251bcdfd31b197d76b19e1d8dff2a92344130e72
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-07-22 18:11:06 -0400

    setup the imports correctly

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import App from '../App'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 export { Home, About, Contact, Navigation };
